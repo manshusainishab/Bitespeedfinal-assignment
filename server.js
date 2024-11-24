de80f18b7ca5ed9fe68cd9d0d60b6ee97560b88c
manshusainishab <manshupallav@gmail.com>
2024-11-24 15:31:22 +0530

    correcting port

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const identifyContact = require('./main');
 const app = express();
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5005;
 
 app.post('/identify', identifyContact);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' ,() => {
     console.log(`Server running on port ${PORT}`);
 })
 
