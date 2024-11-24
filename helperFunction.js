9e3260ea969cac38ff8cb68ae0e62a9873813bf1
manshusainishab <manshupallav@gmail.com>
2024-11-24 05:52:11 +0530

    renamed table based on assig. and added .gitignore

--- helperFunction.js
+++ helperFunction.js
@@ -2,21 +2,21 @@ const db = require('./db');
 
 const getByEmailOrPhone = async (email, phoneNumber) => {
     const [records] = await db.execute(
-        `select * from Contact where email = '${email}' or phoneNumber = '${phoneNumber}'`
+        `select * from Contacts where email = '${email}' or phoneNumber = '${phoneNumber}'`
     );
     return records;
 };
 
 const getByEmailAndPhone = async (email, phoneNumber) => {
     const [records] = await db.execute(
-        `select * from Contact where email = '${email}' and phoneNumber = '${phoneNumber}'`
+        `select * from Contacts where email = '${email}' and phoneNumber = '${phoneNumber}'`
     );
     return records;
 };
 
 const addContact = async (email, phoneNumber, linkedId = null, linkPrecedence = 'primary') => {
     const [newRecord] = await db.execute(
-        `insert into Contact (email, phoneNumber, linkedId, linkPrecedence, createdAt, updatedAt) values(?, ?, ?, ?, now(), now())`,
+        `insert into Contacts (email, phoneNumber, linkedId, linkPrecedence, createdAt, updatedAt) values(?, ?, ?, ?, now(), now())`,
         [email, phoneNumber, linkedId, linkPrecedence]
     );
     return newRecord.insertId;
@@ -25,18 +25,18 @@ const addContact = async (email, phoneNumber, linkedId = null, linkPrecedence = 
 const updateById = async (id, updates) => {
     const keysToUpdate = Object.keys(updates).map(key => `${key} = ?`).join(', ');
     const values = [...Object.values(updates), id];
-    await db.execute(`update Contact set ${keysToUpdate} where id = ?`, values);
+    await db.execute(`update Contacts set ${keysToUpdate} where id = ?`, values);
 };
 
 const updateByLinkedId = async (linkedId, updates) => {
     const keysToUpdate = Object.keys(updates).map(key => `${key} = ?`).join(', ');
     const values = [...Object.values(updates), linkedId];
-    await db.execute(`update Contact set ${keysToUpdate} where linkedId = ?`, values);
+    await db.execute(`update Contacts set ${keysToUpdate} where linkedId = ?`, values);
 };
 
 const getByLinkedId = async (primaryId) => {
     const [rows] = await db.execute(
-        `select * from Contact where (id = ? OR linkedId = ?)`,
+        `select * from Contacts where (id = ? OR linkedId = ?)`,
         [primaryId, primaryId]
     );
     return rows;
@@ -44,14 +44,14 @@ const getByLinkedId = async (primaryId) => {
 
 const getByEmail = async (email) => {
     const [emails] = await db.execute(
-        `select * from Contact where email = ?`, [email]
+        `select * from Contacts where email = ?`, [email]
     );
     return emails;
 };
 
 const getByPhone = async (phone) => {
     const [phones] = await db.execute(
-        `select * from Contact where phoneNumber = ?`, [phone]
+        `select * from Contacts where phoneNumber = ?`, [phone]
     );
     return phones;
 };
